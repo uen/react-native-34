4a054da2169c4f085b6165648a1290b3ea8b3751
Manolis Vrondakis <manolisvrondakis@gmail.com>
2021-02-06 17:45:24 +0000

    OB cookie jar

--- ReactAndroid/src/main/java/com/facebook/react/modules/network/NetworkingModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/network/NetworkingModule.java
@@ -49,6 +49,108 @@ import okio.ByteString;
 import okio.GzipSource;
 import okio.Okio;
 
+
+import java.util.Map;
+import java.net.CookieHandler;
+import java.net.HttpCookie;
+import java.util.Collections;
+
+import okhttp3.internal.annotations.EverythingIsNonNull;
+import okhttp3.internal.platform.Platform;
+import okhttp3.HttpUrl;
+import okhttp3.Cookie;
+
+import static okhttp3.internal.Util.delimiterOffset;
+import static okhttp3.internal.Util.trimSubstring;
+import static okhttp3.internal.platform.Platform.WARN;
+
+
+
+@EverythingIsNonNull
+final class OBJavaNetCookieJar implements CookieJar {
+  private final CookieHandler cookieHandler;
+
+  public OBJavaNetCookieJar(CookieHandler cookieHandler) {
+    this.cookieHandler = cookieHandler;
+  }
+
+  @Override public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+    if (cookieHandler != null) {
+      List<String> cookieStrings = new ArrayList<>();
+      for (Cookie cookie : cookies) {
+        cookieStrings.add(cookie.toString());
+      }
+      Map<String, List<String>> multimap = Collections.singletonMap("Set-Cookie", cookieStrings);
+      try {
+        cookieHandler.put(url.uri(), multimap);
+      } catch (IOException e) {
+        Platform.get().log(WARN, "Saving cookies failed for " + url.resolve("/..."), e);
+      }
+    }
+  }
+
+  @Override public List<Cookie> loadForRequest(HttpUrl url) {
+    // The RI passes all headers. We don't have 'em, so we don't pass 'em!
+    Map<String, List<String>> headers = Collections.emptyMap();
+    Map<String, List<String>> cookieHeaders;
+    try {
+      cookieHeaders = cookieHandler.get(url.uri(), headers);
+    } catch (IOException e) {
+      Platform.get().log(WARN, "Loading cookies failed for " + url.resolve("/..."), e);
+      return Collections.emptyList();
+    }
+
+    List<Cookie> cookies = null;
+    for (Map.Entry<String, List<String>> entry : cookieHeaders.entrySet()) {
+      String key = entry.getKey();
+      if (("Cookie".equalsIgnoreCase(key) || "Cookie2".equalsIgnoreCase(key))
+        && !entry.getValue().isEmpty()) {
+        for (String header : entry.getValue()) {
+          if (cookies == null) cookies = new ArrayList<>();
+          cookies.addAll(decodeHeaderAsJavaNetCookies(url, header));
+        }
+      }
+    }
+
+    return cookies != null
+      ? Collections.unmodifiableList(cookies)
+      : Collections.emptyList();
+  }
+
+  /**
+   * Convert a request header to OkHttp's cookies via {@link HttpCookie}. That extra step handles
+   * multiple cookies in a single request header, which {@link Cookie#parse} doesn't support.
+   */
+  private List<Cookie> decodeHeaderAsJavaNetCookies(HttpUrl url, String header) {
+    List<Cookie> result = new ArrayList<>();
+    for (int pos = 0, limit = header.length(), pairEnd; pos < limit; pos = pairEnd + 1) {
+      pairEnd = delimiterOffset(header, pos, limit, ";");
+      int equalsSign = delimiterOffset(header, pos, pairEnd, '=');
+      String name = trimSubstring(header, pos, equalsSign);
+      if (name.startsWith("$")) continue;
+
+      // We have either name=value or just a name.
+      String value = equalsSign < pairEnd
+        ? trimSubstring(header, equalsSign + 1, pairEnd)
+        : "";
+
+      // If the value is "quoted", drop the quotes.
+      if (value.startsWith("\"") && value.endsWith("\"")) {
+        value = value.substring(1, value.length() - 1);
+      }
+
+      result.add(new Cookie.Builder()
+        .name(name)
+        .value(value)
+        .domain(url.host())
+        .build());
+    }
+    return result;
+  }
+}
+
+
+
 /** Implements the XMLHttpRequest JavaScript interface. */
 @ReactModule(name = NetworkingModule.NAME)
 public final class NetworkingModule extends NativeNetworkingAndroidSpec {
@@ -181,7 +283,7 @@ public final class NetworkingModule extends NativeNetworkingAndroidSpec {
 
   @Override
   public void initialize() {
-    mCookieJarContainer.setCookieJar(new JavaNetCookieJar(mCookieHandler));
+    mCookieJarContainer.setCookieJar(new OBJavaNetCookieJar(mCookieHandler));
   }
 
   @Override
